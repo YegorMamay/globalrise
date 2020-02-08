f4d0e071fa385a441820e8b14036084f5e3f94d3
Pavel <ponomarenko.pavel.web@gmail.com>
2020-02-08 22:38:57 +0200

    style for first screen, bg for content

--- page-home.php
+++ page-home.php
@@ -5,9 +5,25 @@
 ?>
 
 <?php get_header(); ?>
-<div class="container">
+<?php
+$attachment_elem_id = get_post_meta(get_the_ID(), 'main_image', true);
+$attachment_image = wp_get_attachment_url($attachment_elem_id);
+$main_content = get_field('main_content');
+?>
+<div class="top-section" style="background: url('<?php echo $attachment_image; ?>') no-repeat center / cover">
+    <div class="container">
+        <div class="top-section__wrapper">
+            <div class="top-section__description"><?php echo $main_content['main_description']; ?></div>
+            <h1 class="top-section__title"><?php echo $main_content['main_title']; ?></h1>
+            <button type="button" class="top-section__button js-order"><?php echo $main_content['main_button_text']; ?></button>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="container">
 
-<?php get_template_part('loops/content', 'home'); ?>
+        <?php get_template_part('loops/content', 'home'); ?>
 
-</div><!-- /.container -->
+    </div>
+</div>
 <?php get_footer(); ?>
